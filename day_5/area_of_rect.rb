ccc57d21ba0664093e9db9ee2af61aeafa2a1268
Nikita C <nikita149156@gmail.com>
2021-05-07 17:13:08 +0530

    area_and_perimeter

---
+++ day_5/area_of_rect.rb
@@ -0,0 +1,16 @@
+#find area of rectangle
+puts "Enter the length of rectangle"
+length = gets.chomp.to_f
+puts "Enter the breadth of rectangle"
+breadth = gets.chomp.to_f
+puts "Area of rectangle #{length*breadth}"
+#perimeter of rectangle
+puts "perimeter of rectangle #{2*(length+breadth)}"
+#find area of square
+ puts "Enter the side square"
+ side = gets.chomp.to_f
+ puts "area of square #{2*side}"
+ #perimeter of square
+ puts "perimeter of square #{4*side}"
+
+
