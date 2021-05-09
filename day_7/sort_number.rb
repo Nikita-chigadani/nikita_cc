635b696feaf3937ebb4b2867a6c18df3874e2229
Nikita C <nikita149156@gmail.com>
2021-05-09 16:50:23 +0530

    sorting

---
+++ day_7/sort_number.rb
@@ -0,0 +1,9 @@
+#sort number in ascending order
+numbers = [5,6,3,2,4]
+puts "Given Array"
+puts numbers
+puts "Ascending order"
+print numbers.sort
+puts " "
+puts "Desending order"
+print numbers.sort.reverse
